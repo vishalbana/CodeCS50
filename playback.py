58a9453cbca0f55bd5cdd6dd6b6abe3e8532b9e7
Vishal Bana <111978209+vishalbana@users.noreply.github.com>
2022-11-29 22:07:54 +0530

    Create playback.py

---
+++ playback.py
@@ -0,0 +1,7 @@
+"""
+In a file called playback.py, implement a program in Python that prompts the user for input and then outputs that same input,
+replacing each space with ... (i.e., three periods).
+"""
+
+A = input()
+print(A.replace(" ","..."))
