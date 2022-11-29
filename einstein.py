d6c0eedc1a42df9e957e74b332ae370d3e353c0d
Vishal Bana <111978209+vishalbana@users.noreply.github.com>
2022-11-29 22:02:19 +0530

    Create einstein.py

---
+++ einstein.py
@@ -0,0 +1,11 @@
+"""
+In a file called einstein.py, implement a program in Python that prompts the user for mass as an integer (in kilograms)
+and then outputs the equivalent number of Joules as an integer. Assume that the user will input an integer.
+"""
+
+def main():
+    a = int(input('m:  '))
+    z = (a * 90000000000000000)
+    print('E: ', z)
+
+main()
