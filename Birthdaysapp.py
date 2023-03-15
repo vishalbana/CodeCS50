97df3fb5a5f9ea6358e3277beea93ff30d4c4671
Vishal Bana <111978209+vishalbana@users.noreply.github.com>
2023-03-15 15:38:17 +0530

    Create Birthdaysapp.py
    application file for birthday page implementation

---
+++ Birthdaysapp.py
@@ -0,0 +1,48 @@
+import os
+
+from cs50 import SQL
+from flask import Flask, flash, jsonify, redirect, render_template, request, session
+from helpers import apology
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///birthdays.db")
+
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        name = request.form.get("name")
+        month = int(request.form.get("month"))
+        day = request.form.get("day")
+
+        db.execute("INSERT INTO birthdays (name, month, day) VALUES (?, ?, ?)", name, month, day)
+
+
+        # TODO: Add the user's entry into the database
+
+
+        return redirect("/")
+
+    else:
+
+        # TODO: Display the entries in the database on index.html
+        all_birthdays = db.execute("SELECT name, month, day FROM birthdays")
+
+        return render_template("index.html", all_birthdays=all_birthdays)
+
+
